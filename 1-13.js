4807e909b81503cf87bc175d6b49b77a3911b061
maumau613081 <maumau613081@gmail.com>
2026-01-20 08:31:07 +0900

    update

--- 1-13.js
+++ 1-13.js
@@ -1,1 +1,15 @@
+let digits=2;
+let times=3;
 
+function makequestion(){
+    let question = "";
+    for(let i=0; i<times; i++){
+        const number = Math.floor(Math.random() * (10 ** digits));
+        question += `${number} + `;
+    }
+    let questionstr = question.slice(0, -3);
+    questionstr += " = ";
+    console.log(questionstr);
+}
+
+makequestion()
